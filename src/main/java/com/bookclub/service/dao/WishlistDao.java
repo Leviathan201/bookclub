bf1e20004b28f62a44d82a5132eab08877a03883
Leviathan201 <90806079+Leviathan201@users.noreply.github.com>
2022-04-10 18:02:06 -0400

    Add files via upload

---
+++ src/main/java/com/bookclub/service/dao/WishlistDao.java
@@ -0,0 +1,15 @@
+/*
+Liang, Y.D. (2019). Introduction to Java Programming and Data Structures:
+Comprehensive Version (12th ed.). Pearson Education, Inc.
+Modifications by R. Krasso, 2021
+Additional Modifications by James Tabor
+ */
+
+package com.bookclub.service.dao;
+
+import com.bookclub.model.WishlistItem;
+import com.bookclub.service.GenericDao;
+
+//Extends the users interface to include wishlist
+public interface WishlistDao extends GenericDao<WishlistItem, String> {
+}
