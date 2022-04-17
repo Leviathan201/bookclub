5747aded3adf22880e97998fea9ed0ac31062ef0
Leviathan201 <90806079+Leviathan201@users.noreply.github.com>
2022-04-16 20:45:27 -0400

    Add files via upload

---
+++ src/main/java/com/bookclub/service/GenericCrudDao.java
@@ -0,0 +1,18 @@
+/*
+Liang, Y.D. (2019). Introduction to Java Programming and Data Structures:
+Comprehensive Version (12th ed.). Pearson Education, Inc.
+Modifications by R. Krasso, 2021
+Additional Modifications by James Tabor
+ */
+
+package com.bookclub.service;
+
+import java.util.List;
+
+public interface GenericCrudDao<E, K> {
+    void add(E entity);
+    void update(E entity);
+    boolean remove(E entity);
+    List<E> list(); // Return a list of objects of type E.
+    E find(K key); // Return an object of type E by type K value.
+}
