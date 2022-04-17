687a275a0ed06d9e1e853a62b824af2fd467b055
Leviathan201 <90806079+Leviathan201@users.noreply.github.com>
2022-04-16 20:45:50 -0400

    Add files via upload

--- src/main/java/com/bookclub/service/dao/WishlistDao.java
+++ src/main/java/com/bookclub/service/dao/WishlistDao.java
@@ -8,8 +8,9 @@ Additional Modifications by James Tabor
 package com.bookclub.service.dao;
 
 import com.bookclub.model.WishlistItem;
+import com.bookclub.service.GenericCrudDao;
 import com.bookclub.service.GenericDao;
 
 //Extends the users interface to include wishlist
-public interface WishlistDao extends GenericDao<WishlistItem, String> {
+public interface WishlistDao extends GenericCrudDao<WishlistItem, String> {
 }
