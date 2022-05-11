abad525e87637dab7c029d70d0adce4e75179117
Leviathan201 <90806079+Leviathan201@users.noreply.github.com>
2022-05-11 18:19:29 -0400

    Add files via upload

--- src/main/java/com/bookclub/web/WishlistController.java
+++ src/main/java/com/bookclub/web/WishlistController.java
@@ -14,8 +14,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.security.core.Authentication;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -57,5 +59,35 @@ public class WishlistController {
         wishlistDao.add(wishlistItem);
 
         return "redirect:/wishlist"; //Return back to the wishlist
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/remove/{id}")
+    public String removeWishlistItem(@PathVariable String id) {
+
+        wishlistDao.remove(id); //Removing item from wishlist
+
+        return "redirect:/wishlist";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/{id}")
+    public String showWishlistItem(@PathVariable String id, Model model) {
+        WishlistItem wishlistItem = wishlistDao.find(id);
+
+        model.addAttribute("wishlistItem", wishlistItem); //Shows user wishlist
+
+        return "wishlist/view";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, path = "/update")
+    public String updateWishlistItem(@Valid WishlistItem wishlistItem, BindingResult bindingResult, Authentication authentication) {
+        wishlistItem.setUsername(authentication.getName()); //Get wishlist based off username
+
+        if (bindingResult.hasErrors()) {
+            return "wishlist/view";
+        }
+
+        wishlistDao.update(wishlistItem); //Update wishlist based off username
+
+        return "redirect:/wishlist";
     }
 }
--- src/main/java/com/bookclub/web/WishlistRestController.java
+++ src/main/java/com/bookclub/web/WishlistRestController.java
@@ -1,9 +1,17 @@
+/*
+Liang, Y.D. (2019). Introduction to Java Programming and Data Structures:
+Comprehensive Version (12th ed.). Pearson Education, Inc.
+Modifications by R. Krasso, 2021
+Additional Modifications by James Tabor
+ */
+
 package com.bookclub.web;
 
 import com.bookclub.model.WishlistItem;
 import com.bookclub.service.dao.WishlistDao;
 import com.bookclub.service.impl.MongoWishlistDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,11 +27,12 @@ public class WishlistRestController {
         this.wishlistDao = wishlistDao; //Setter method for the wishlist dao
     }
 
-    @RequestMapping(method = RequestMethod.GET)
-    public List<WishlistItem> showWishlist() {
-        return wishlistDao.list(); //Returns an item from the wishlist
-    }
+    @RequestMapping(method = RequestMethod.GET) //Finds list based off the authenttication of the username
+    public List<WishlistItem> showWishlist(Authentication authentication) {
+        String username = authentication.getName();
 
+        return wishlistDao.list(username);
+    }
     @RequestMapping(path = "/{id}", method = RequestMethod.GET)
     public WishlistItem findById(@PathVariable String id) {
         return wishlistDao.find(id); //Finds the wishlist item based on the ID
