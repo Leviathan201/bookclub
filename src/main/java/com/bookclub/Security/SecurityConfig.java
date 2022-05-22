da774232edc8d7c1db7b24a17c4785f55a5803e9
Leviathan201 <90806079+Leviathan201@users.noreply.github.com>
2022-05-22 19:43:05 -0400

    Add files via upload

--- src/main/java/com/bookclub/Security/SecurityConfig.java
+++ src/main/java/com/bookclub/Security/SecurityConfig.java
@@ -1,10 +1,19 @@
+/*
+Liang, Y.D. (2019). Introduction to Java Programming and Data Structures:
+Comprehensive Version (12th ed.). Pearson Education, Inc.
+Modifications by R. Krasso, 2021
+Additional Modifications by James Tabor
+ */
+
 package com.bookclub.Security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web
+        .configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web
+        .configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -14,28 +23,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder(); //Creates a random password to login to the bookclub web
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 
         auth
                 .inMemoryAuthentication()
                 .withUser("user").password(encoder.encode("password")).roles("USER")
                 .and()
-                .withUser("testuser01").password(encoder.encode("password02")).roles("USER", "ADMIN"); //Created a password and username here
+                .withUser("testuser01").password(encoder.encode("password01")).roles("USER", "ADMIN");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().authenticated() //Authenticates a request
+                .antMatchers("/monthly-books/list", "/monthly-books/new", "/monthly-books").hasRole("ADMIN") //Updated the security config to include the admin role
+                .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login") //Brings you to the login page
+                .loginPage("/login")
                 .permitAll()
                 .and()
                 .logout()
-                .logoutSuccessUrl("/login?logout=true") //Logs the user out successfully
-                .invalidateHttpSession(true) //Creates a invalid session if user inputs wrong password or user
+                .logoutSuccessUrl("/login?logout=true")
+                .invalidateHttpSession(true)
                 .permitAll();
     }
 }
